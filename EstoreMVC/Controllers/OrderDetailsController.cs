ed5c707ebdbebe4a44dcb4c24a679b934631594e
Huy Vo Quang <huyvqhe163784@fpt.edu.vn>
2024-01-22 13:33:18 +0700

    Add Order Report

---
+++ EstoreMVC/Controllers/OrderDetailsController.cs
@@ -0,0 +1,37 @@
+﻿// EstoreMVC/Controllers/OrderDetailsController.cs
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using EstoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public class OrderDetailsController : Controller
+{
+    private readonly EStoreContext _context;
+
+    public OrderDetailsController(EStoreContext context)
+    {
+        _context = context;
+    }
+
+    // ... Other actions ...
+
+    [HttpGet]
+    public async Task<IActionResult> Report(DateTime? fromDate, DateTime? toDate)
+    {
+        fromDate ??= DateTime.Today.AddMonths(-1);
+        toDate ??= DateTime.Today;
+
+        var orderDetails = await _context.OrderDetails
+            .Include(od => od.Product)
+            .Where(od => od.Order.OrderDate >= fromDate && od.Order.OrderDate <= toDate)
+            .ToListAsync();
+
+        ViewBag.OrderCount = orderDetails.Count();
+        ViewBag.FromDate = fromDate.Value;
+        ViewBag.ToDate = toDate.Value;
+
+        return View(orderDetails);
+    }
+}
